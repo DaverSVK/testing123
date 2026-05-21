b7ef8b6e1c5e71ba904a29f9e4844f5354e31b1a
Daver <david.szepvolgyi@gmail.com>
2026-05-21 21:59:13 +0200

    Add Raspberry Pi builder job 5

--- Src/main.c
+++ Src/main.c
@@ -76,10 +76,10 @@ int main(void)
 	  else
 	  {
 		  // 1s delay
-		  LL_mDelay(2000);
+		  LL_mDelay(1000);
 		  LED_ON;
 		  // 1s delay
-		  LL_mDelay(2000);
+		  LL_mDelay(1000);
 		  LED_OFF;
 	  }
   }
