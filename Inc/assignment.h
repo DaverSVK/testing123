1df1006be563a783bce63bb4a822db488de0d64f
Daver <david.szepvolgyi@gmail.com>
2026-05-26 13:20:49 +0200

    zadanie 2 test

--- Inc/assignment.h
+++ Inc/assignment.h
@@ -8,58 +8,46 @@
 #ifndef ASSIGNMENT_H_
 #define ASSIGNMENT_H_
 
-#include <stdint.h>
-
 /**
  * 		This header file provides macros to the MCU's registers required for this assignment.
  * 		Your task is to provide their actual implementation so you can use them in application in "main.c"
  * 		and make your "LED blink" application code readable and great again!
  */
 
+/* Register access macro */
+#define REG32(addr) ((volatile uint32_t *)(addr))
+
 /* General purpose input output port A macros */
 //GPIOA peripheral base address
-#define	GPIOA_BASE_ADDR			0x48000000
+#define	GPIOA_BASE_ADDR			0x48000000UL/* Add GPIO A peripheral base address here. */
 //MODER register
-#define	GPIOA_MODER_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x00))
+#define	GPIOA_MODER_REG			REG32(GPIOA_BASE_ADDR + 0x00UL)/* Add moder register address here. */
 //OTYPER register
-#define	GPIOA_OTYPER_REG		((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x04))
+#define	GPIOA_OTYPER_REG		REG32(GPIOA_BASE_ADDR + 0x04UL)/* Add otyper register address here. */
 //OSPEEDER register
-#define GPIOA_OSPEEDER_REG		((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x08))
+#define GPIOA_OSPEEDER_REG		REG32(GPIOA_BASE_ADDR + 0x08UL)/* Add ospeeder register address here. */
 //PUPDR register
-#define GPIOA_PUPDR_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x0C))
+#define GPIOA_PUPDR_REG			REG32(GPIOA_BASE_ADDR + 0x0CUL)/* Add pupdr register address here. */
 //IDR register
-#define GPIOA_IDR_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x10))
+#define GPIOA_IDR_REG			REG32(GPIOA_BASE_ADDR + 0x10UL)/* Add idr register address here. */
 //ODR register
-#define GPIOA_ODR_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x14))
+#define GPIOA_ODR_REG			REG32(GPIOA_BASE_ADDR + 0x14UL)/* Add odr register address here. */
 //BSRR register
-#define GPIOA_BSRR_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x18))
+#define GPIOA_BSRR_REG			REG32(GPIOA_BASE_ADDR + 0x18UL)/* Add bsrr register address here. */
 //BRR register
-#define GPIOA_BRR_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x28))
-
-/* GPIOA Alternate Function Low register */
-#define GPIOA_AFRL_REG			((volatile uint32_t *)(GPIOA_BASE_ADDR + 0x20))
+#define GPIOA_BRR_REG			REG32(GPIOA_BASE_ADDR + 0x28UL)/* Add brr register address here. */
 
 /*Reset clock control register macros */
 //RCC base address
-#define	RCC_BASE_ADDR			0x40021000
+#define	RCC_BASE_ADDR			0x40021000UL/* Add rcc register address here. */
 //AHBEN register
-#define	RCC_AHBENR_REG			((volatile uint32_t *)(RCC_BASE_ADDR + 0x14))
-//APB1ENR register
-#define	RCC_APB1ENR_REG			((volatile uint32_t *)(RCC_BASE_ADDR + 0x1C))
-
-/* USART2 registers (debug UART – ST-Link VCP, PA2=TX) */
-#define USART2_BASE_ADDR		0x40004400
-#define USART2_CR1_REG			((volatile uint32_t *)(USART2_BASE_ADDR + 0x00))
-#define USART2_BRR_REG			((volatile uint32_t *)(USART2_BASE_ADDR + 0x0C))
-#define USART2_ISR_REG			((volatile uint32_t *)(USART2_BASE_ADDR + 0x1C))
-#define USART2_TDR_REG			((volatile uint32_t *)(USART2_BASE_ADDR + 0x28))
-
+#define	RCC_AHBENR_REG			REG32(RCC_BASE_ADDR + 0x14UL)/* Add ahben register address here. */
 
 /* LED and button macros */
-#define LED_ON					(*GPIOA_ODR_REG |=  (1u << 4))
-#define LED_OFF					(*GPIOA_ODR_REG &= ~(1u << 4))
+#define LED_ON					(*GPIOA_BSRR_REG = (1UL << 4))/* Add LED_ON implementation here. */
+#define LED_OFF					(*GPIOA_BRR_REG = (1UL << 4))/* Add LED_OFF implementation here. */
 
-#define BUTTON_GET_STATE		(*GPIOA_IDR_REG &   (1u << 3))
+#define BUTTON_GET_STATE		(*GPIOA_IDR_REG & (1UL << 3))/* Add BUTTON_GET_STATE implementation here. */
 
 
 #endif /* ASSIGNMENT_H_ */
--- Src/main.c
+++ Src/main.c
@@ -22,14 +22,6 @@
 #include "main.h"
 #include "assignment.h"
 
-static void uart_send_str(const char *s)
-{
-	while (*s) {
-		while (!(*USART2_ISR_REG & (1u << 7)));
-		*USART2_TDR_REG = (uint32_t)(*s++);
-	}
-}
-
 int main(void)
 {
   /*
@@ -55,50 +47,24 @@ int main(void)
 
   /* Enable clock for GPIO port A*/
 
-	*RCC_AHBENR_REG |= (1u << 17);		/* IOPAEN – enable GPIOA peripheral clock */
-
-  /* Enable USART2 clock (APB1ENR bit 17) */
-	*RCC_APB1ENR_REG |= (1u << 17);
-
-  /* PA2 – USART2_TX: alternate function mode (MODER[5:4] = 10) */
-	*GPIOA_MODER_REG &= ~(0x3u << 4);
-	*GPIOA_MODER_REG |=  (0x2u << 4);
-
-  /* PA2 – AF7 (USART2_TX): AFRL[11:8] = 0111 */
-	*GPIOA_AFRL_REG &= ~(0xFu << 8);
-	*GPIOA_AFRL_REG |=  (0x7u << 8);
-
-  /* USART2 – 115200 baud @ 8 MHz: BRR = fck / baud = 8000000 / 115200 = 69 */
-	*USART2_BRR_REG = 69u;
-	*USART2_CR1_REG = (1u << 3) | (1u << 0);	/* TE | UE */
-
-  /* Send greeting over debug UART */
-	uart_send_str("Hello!\r\n");
+	//type your code for GPIOA clock enable here:
 
 
   /* GPIOA pin 3 and 4 setup */
 
-	/* PA3 – input (button): MODER bits [7:6] = 00, pull-up PUPDR bits [7:6] = 01 */
-	*GPIOA_MODER_REG &= ~(0x3u << 6);
-	*GPIOA_PUPDR_REG &= ~(0x3u << 6);
-	*GPIOA_PUPDR_REG |=  (0x1u << 6);
-
-	/* PA4 – output (LED): MODER bits [9:8] = 01 (general purpose output), push-pull */
-	*GPIOA_MODER_REG &= ~(0x3u << 8);
-	*GPIOA_MODER_REG |=  (0x1u << 8);
+	//type your code for GPIOA pins setup here:
 
 
   while (1)
   {
 	  if(BUTTON_GET_STATE)
 	  {
-		  // 0.25s delay sadasd
+		  // 0.25s delay
 		  LL_mDelay(250);
 		  LED_ON;
 		  // 0.25s delay
 		  LL_mDelay(250);
 		  LED_OFF;
-		  uart_send_str("Hello World!\r\n");
 	  }
 	  else
 	  {
